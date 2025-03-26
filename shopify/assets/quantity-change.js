22ea0bd4e59032330ba537873b522be7f20b9c0a
nairagrig99 <nairagrig1999@gmail.com>
2025-03-26 11:11:09 +0400

    add shopify project with product pag and cart functionality

---
+++ shopify/assets/quantity-change.js
@@ -0,0 +1,37 @@
+class QuantityChange extends HTMLElement {
+    static observedAttributes = ["color", "size"];
+
+    constructor() {
+        super();
+    }
+
+
+    connectedCallback() {
+        console.log('HTMLElement', this.querySelector('.quantity-decrease'))
+        this.querySelector('.quantity-decrease')?.addEventListener("click", () => this.updateQuantity(-1))
+        this.querySelector('.quantity-increase')?.addEventListener("click", () => this.updateQuantity(1))
+    }
+
+    updateQuantity(amount) {
+        const quantityEl = this.querySelector("#quantity");
+        const quantityElValue = quantityEl.value;
+        let quantity = parseInt(quantityElValue) + amount;
+        if (quantity < 1) quantity = 1;
+        quantityEl.value = quantity;
+    }
+
+    disconnectedCallback() {
+        console.log("Custom element removed from page.");
+    }
+
+    adoptedCallback() {
+        console.log("Custom element moved to new page.");
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`Attribute ${name} has changed.`);
+    }
+
+}
+
+customElements.define("quantity-change", QuantityChange)
